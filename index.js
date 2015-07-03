46094d47c2011f0a59f270451afef4916a83841f
dilloncortez <dcortez@peer60.com>
2015-07-03 12:07:34 -0600

    added card display

--- index.js
+++ index.js
@@ -16,10 +16,36 @@
     };
     $scope.game = g;
   }
-
   blackjackController.$inject = ["$scope", "Game"];
-
   app.controller("blackjack", blackjackController);
+
+  function cardDirective($sce) {
+    return {
+      scope: {
+        card: '@val'
+      },
+      restrict: 'E',
+      template: "<div class='card' ng-click='hit()'>{{card[1]}} <span ng-bind-html='symbol'></span></div>",
+      link: function link(scope, element, attr){
+        // debugger;
+        scope.suit = scope.card[0];
+        if(scope.suit === 'H'){
+          element.css("color", "red");
+          scope.symbol = $sce.trustAsHtml("&hearts;");
+        }else if(scope.suit === "D"){
+          scope.symbol = $sce.trustAsHtml("&diams;");
+          element.css("color", "red");
+        } else if(scope.suit === "S"){
+          scope.symbol = $sce.trustAsHtml("&spades;");
+        } else if(scope.suit === "C"){
+          scope.symbol = $sce.trustAsHtml("&clubs;");
+        }
+      }
+    };
+  }
+  cardDirective.$inject = ["$sce"];
+
+  app.directive('card', cardDirective);
 
   angular.bootstrap(document.body, ["app"], {strictDi: true});
 }());
